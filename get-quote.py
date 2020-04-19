105584d2c1806e9da2e3f3f874cbc93a673dee65
xkey10x <57377849+xkey10x@users.noreply.github.com>
2020-04-18 18:13:29 -0600

    "Randomized the output of array quotes.txt"

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,13 @@
+import random
 def freedom():
-   print("Keep it logically awesome.")
+#   print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
-
-  #print(quotes)
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
+  last = 13
+  rnd = random.randint(0, last)
+  print(quotes[rnd])
 
 if __name__== "__main__":
   freedom()
